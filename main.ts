7f04fcbdaf6b0c1e201efd035a25440549ea76fa
Nicolas Beaussart <nic.beaussart@gmail.com>
2021-04-16 22:06:51 +0200

    Cleenup code

--- main.ts
+++ main.ts
@@ -1,93 +1,114 @@
-import {
-  App,
-  Modal,
-  Notice,
-  Plugin,
-  PluginSettingTab,
-  Setting,
-  TFile,
-} from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import matter from 'gray-matter';
-import { add, parse, formatRFC3339, isAfter, parseISO } from 'date-fns';
+import { add, parse, formatRFC3339, isAfter } from 'date-fns';
 
-interface MyPluginSettings {
-  header: string;
+interface UpdateTimeOnEditSettings {
+  headerUpdated: string;
+  headerCreated: string;
   minMinutesBetweenSaves: number;
+  ignoreFolder?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-  header: 'updated',
+const DEFAULT_SETTINGS: UpdateTimeOnEditSettings = {
+  headerUpdated: 'updated',
+  headerCreated: 'created',
   minMinutesBetweenSaves: 1,
+  ignoreFolder: undefined,
 };
 
-export default class MyPlugin extends Plugin {
-  settings: MyPluginSettings;
+export default class UpdateTimeOnSavePlugin extends Plugin {
+  settings: UpdateTimeOnEditSettings;
 
-  parseDate(input: string | Date | undefined): Date {
-    if (!input) {
-      return new Date(0);
-    }
+  parseDate(input: string | Date): Date {
     if (input instanceof Date) {
       return input;
     }
-    const oldFormat = 'yyyy-MM-dd HH:mm-ssxxx';
-    if (input.contains('T')) {
-      return new Date(input);
-    }
-    return parse(input, oldFormat, new Date());
+    return new Date(input);
   }
 
   async onload() {
-    console.log('loading Update time on edit plugin');
+    this.log('loading plugin');
 
     await this.loadSettings();
 
+    this.setupOnEditHandler();
+
+    this.addSettingTab(new UpdateTimeOnEditSettingsTab(this.app, this));
+  }
+
+  shouldFileBeIgnored(path: string): boolean {
+    if (!this.settings.ignoreFolder) {
+      return false;
+    }
+    return !path.startsWith(this.settings.ignoreFolder);
+  }
+
+  shouldUpdateValue(date: Date): boolean {
+    const nMinutesAgo = add(new Date(), {
+      minutes: -this.settings.minMinutesBetweenSaves,
+    });
+    return isAfter(nMinutesAgo, date);
+  }
+
+  setupOnEditHandler() {
     this.app.vault.on('modify', async (file) => {
       const oldText = (file as any).unsafeCachedData;
       if (!oldText) {
         return;
       }
-      const { content, data } = matter(oldText);
-      data.updated = this.parseDate(data.updated);
-      data.created = data.created ? this.parseDate(data.created) : new Date();
 
-      const nMinutesAgo = add(new Date(), {
-        minutes: -this.settings.minMinutesBetweenSaves,
-      });
-      const shouldEdit = isAfter(nMinutesAgo, data.updated);
-
-      if (!shouldEdit) {
-        console.log('Not soon enouth, will update latter');
+      if (this.shouldFileBeIgnored(file.path)) {
         return;
       }
 
-      data.created = formatRFC3339(data.created);
-      data.updated = formatRFC3339(new Date());
+      const { content, data } = matter(oldText);
+
+      const updatedKey = this.settings.headerUpdated;
+      const createdKey = this.settings.headerCreated;
+
+      // Set the creation date as now if there is no entry in the front matter
+      data[createdKey] = data[createdKey]
+        ? this.parseDate(data[createdKey])
+        : new Date();
+
+      // Set the update date as epoch if there is no entry in the front matter
+      data[updatedKey] = data[updatedKey]
+        ? this.parseDate(data[updatedKey])
+        : new Date(0);
+
+      if (!this.shouldUpdateValue(data[updatedKey])) {
+        this.log('Not soon enough, will update latter');
+        return;
+      }
+
+      data[createdKey] = formatRFC3339(data[createdKey]);
+      data[updatedKey] = formatRFC3339(new Date());
 
       const newData = matter.stringify(content, data);
 
-      console.log(newData);
-
+      // Get the file for the modify parameter
       const fileToSave = this.app.vault
         .getFiles()
         .find((inFile) => inFile.path === file.path);
 
-      console.log(fileToSave);
-      console.log(file.path);
-
       await this.app.vault.modify(fileToSave, newData);
-      console.log('Updated !');
+      this.log('Document updated !');
     });
-
-    this.addSettingTab(new SampleSettingTab(this.app, this));
   }
 
   onunload() {
-    console.log('unloading plugin');
+    this.log('unloading Update time on edit plugin');
+  }
+
+  log(payload: string) {
+    console.log(`[TIME UPDATER PLUGIN] ${payload}`);
   }
 
   async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    this.settings = {
+      ...DEFAULT_SETTINGS,
+      ...(await this.loadData()),
+    };
   }
 
   async saveSettings() {
@@ -95,10 +116,10 @@ export default class MyPlugin extends Plugin {
   }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-  plugin: MyPlugin;
+class UpdateTimeOnEditSettingsTab extends PluginSettingTab {
+  plugin: UpdateTimeOnSavePlugin;
 
-  constructor(app: App, plugin: MyPlugin) {
+  constructor(app: App, plugin: UpdateTimeOnSavePlugin) {
     super(app, plugin);
     this.plugin = plugin;
   }
@@ -108,22 +129,35 @@ class SampleSettingTab extends PluginSettingTab {
 
     containerEl.empty();
 
-    new Setting(containerEl).setName('Front matter name').addText((text) =>
-      text
-        .setPlaceholder('updated')
-        .setValue(this.plugin.settings.header ?? '')
-        .onChange(async (value) => {
-          this.plugin.settings.header = value;
-          await this.plugin.saveSettings();
-        }),
-    );
+    new Setting(containerEl)
+      .setName('Front matter updated name')
+      .setDesc('The key in the front matter yaml for the update time')
+      .addText((text) =>
+        text
+          .setPlaceholder('updated')
+          .setValue(this.plugin.settings.headerUpdated ?? '')
+          .onChange(async (value) => {
+            this.plugin.settings.headerUpdated = value;
+            await this.plugin.saveSettings();
+          }),
+      );
 
     new Setting(containerEl)
-      .setName('Excluded folder')
-      .addSearch((search) => search);
+      .setName('Front matter created name')
+      .setDesc('The key in the front matter yaml for the creation time')
+      .addText((text) =>
+        text
+          .setPlaceholder('updated')
+          .setValue(this.plugin.settings.headerCreated ?? '')
+          .onChange(async (value) => {
+            this.plugin.settings.headerCreated = value;
+            await this.plugin.saveSettings();
+          }),
+      );
 
     new Setting(containerEl)
       .setName('Min minutes between update')
+      .setDesc('The number of minutes before a new update should appends')
       .addSlider((slider) =>
         slider
           .setLimits(1, 30, 1)
@@ -133,6 +167,19 @@ class SampleSettingTab extends PluginSettingTab {
             await this.plugin.saveSettings();
           })
           .setDynamicTooltip(),
+      );
+
+    new Setting(containerEl)
+      .setName('Folder to exclude')
+      .setDesc('File in this folder will not have the front matter updated')
+      .addText((text) =>
+        text
+          .setPlaceholder('templates')
+          .setValue(this.plugin.settings.ignoreFolder ?? '')
+          .onChange(async (value) => {
+            this.plugin.settings.ignoreFolder = value;
+            await this.plugin.saveSettings();
+          }),
       );
   }
 }
